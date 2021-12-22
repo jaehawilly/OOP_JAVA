4ab41b43b232379559b4976422ac3028382cc6c2
jaehawilly <jaehawilly@naver.com>
2021-12-22 16:45:54 +0900

    first

---
+++ Ex4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        String[] data = { "A", "K", "A", "K", "D", "A",
+                "K", "K", "K", "Z", "D" };
+        HashMap<String, Integer> map = new HashMap<>();
+        Integer value=0;
+
+        for(int i=0;i<data.length;i++){
+            if(map.containsKey(data[i])){
+                value=map.get(data[i]);
+                map.put(data[i], value+1);
+            }
+            else map.put(data[i], new Integer(1));
+        }
+
+        List<String> keyList=new ArrayList<>(map.keySet());
+        Collections.sort(keyList);
+        Iterator<String> itr=keyList.iterator();
+        while(itr.hasNext()){
+            String key=itr.next();
+            System.out.println(key + " : " + map.get(key));
+        }
+    }
+}
